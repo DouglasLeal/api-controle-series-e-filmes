70dda254e478199774ec9f8e6dfc853d6b707999
Douglas Leal <douglas.leal@live.com>
2023-02-13 18:13:35 -0300

    Modelo Midia criado

---
+++ App/Models/Midia.cs
@@ -0,0 +1,25 @@
+﻿namespace App.Models
+{
+    public abstract class Midia
+    {
+        public int Id { get; set; }
+        public string? TituloOriginal { get; set; }
+        public string? TituloBrasileiro { get; set; }
+        public string? Descricao { get; set; }
+        public int Nota { get; set; }
+        public DateOnly? Lancamento { get; set; }
+        public Genero? Genero { get; set; }
+    }
+
+    public enum Genero
+    {
+        Acao,
+        Comedia,
+        Drama,
+        Romance,
+        Documentario,
+        Suspense,
+        Terror,
+        FiccaoCientifica
+    }
+}
